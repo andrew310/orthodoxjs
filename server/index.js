6c02fb93b2ddb999031a53457ce085e73302c9d0
Andrew Brown <andrewbrown600@gmail.com>
2016-06-23 21:59:00 -0700

    handlebars engine

--- server/index.js
+++ server/index.js
@@ -2,11 +2,17 @@ import express from 'express';
 import React from 'react';
 import ReactDomServer from 'react-dom/server';
 import App from './generated/app';
+import handlebars from 'express-handlebars';
 
 const app = express();
 
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-    res.send(ReactDomServer.renderToString(<App />));
+    res.render('app', {
+      app: ReactDomServer.renderToString(<App />)
+    });
 });
 
 app.listen(3000, () => console.log('magic happens on port 3000'));
