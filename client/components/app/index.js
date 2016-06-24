386ba34d06c2900601892925025a4d08de4b64be
Andrew Brown <andrewbrown600@gmail.com>
2016-06-23 22:43:12 -0700

    reverted to teh todo list

--- client/components/app/index.js
+++ client/components/app/index.js
@@ -1,64 +1,111 @@
 import React from 'react';
 import {} from './style.less';
 
-var Hello = React.createClass({
-	handleKey: function(e){
-  	if (e.keyCode == 13){
-    	this.props.keyDown();
-    }
-  },
-  handleChange: function(e){
-    this.props.handleChange(e.target.value);
-  },
-  handleBlur: function(e){
-  	this.props.keyDown();
-  },
-  render: function() {
-  if(this.props.edit != "true"){
-    return (
-    <div onDoubleClick={this.props.doubleClick}>
-    	{this.props.data}
-    </div>
-  );}
-  else{
-  	return (
-    	<input
-      autoFocus="true"
-      onKeyDown={this.handleKey}
-      value={this.props.data}
-      onChange={this.handleChange}
-      onBlur={this.handleBlur} />
-    );}
+/*INPUT FIELDS*/
+var Form = React.createClass({
+	getInitialState: function(){
+		return {title: '', text: ''};
+	},
+	handleTitleChange: function(e){
+		this.setState({title: e.target.value})
+	},
+	handleTextChange: function(e){
+		this.setState({text: e.target.value})
+	},
+	handleSubmit: function(e){
+		e.preventDefault();
+		//alert(this.state.title);
+		this.props.onItemSubmit({title: this.state.title, text: this.state.text})
+		this.setState({title: '', text: ''});
+	},
+	render: function(){
+		return (
+			<form onSubmit={this.handleSubmit}>
+				Title:<br />
+				<input type="text" name="title" value={this.state.title} onChange={this.handleTitleChange} /><br />
+				Text:<br />
+				<input type="text" name="text" value={this.state.text} onChange={this.handleTextChange}/><br />
+				<input type="submit" value="Post" />
+			</form>
+		);
+	}
+})
+
+
+
+/*ITEM TEMPLATE*/
+var Item = React.createClass({
+	handleDelete(){
+		this.props.onDelete(this.props.id);
+	},
+	render: function(){
+		return (
+    	<li className="listItem">
+        <h2 className="listTitle">
+          {this.props.title}
+        </h2>
+        <div>{this.props.text}</div>
+				<button onClick={this.handleDelete}>delete</button>
+      </li>
+    );
   }
 });
 
 
-var App = React.createClass({
-	getInitialState: function(){
-  	return {edit: "false", data: "edit me"};
-  },
-	handleDoubleClick: function(){
-  	this.setState({edit: "true"});
-  },
-  handleKeyDown: function(){
-  	this.setState(
-    	{edit: "false"}
-    );
-  },
-  handleChange: function(text){
-  this.setState({data: text});
-  },
-  render: function() {
-  	return (<
-    	Hello edit={this.state.edit}
-      data={this.state.data}
-      doubleClick={this.handleDoubleClick}
-      keyDown={this.handleKeyDown}
-      handleChange={this.handleChange}
-      />
 
-     );}
-});
+/*LIST OF ITEMS
+ * this component is the renders the list
+ * parent: App
+ * Child: Item
+ * Receives state from parent, maps to child, passes functions to delete as prop
+ */
+var List = React.createClass({
+	render: function(){
+		var deleteMe = this.props.handleDelete;
+  	var listItems = this.props.data.map(function(item, i){
+      return (
+        <Item title={item.title} id={i} text={item.text} onDelete={deleteMe}>
+        </Item>
+      );
+ 	 });
+    return(
+      <ul className="col-md-4 list-group">
+        {listItems}
+      </ul>
+    );
+  }
+})
+
+
+
+/*PARENT CLASS TO PUT THEM ALL TOGETHER*/
+var App = React.createClass({
+	handleDelete: function(itemIdx){
+		//alert(itemIdx);
+		var deleteThis = this.state.data.splice(itemIdx,1)
+      this.setState({data: this.state.data});
+	},
+	getInitialState: function(){
+		return {data: [
+			{id: 1, title: "List Item 1", text: "this is a list item"},
+			{id: 2, title: "List Item 2", text: "this is another list item"}
+		]};
+	},
+	handleSubmit: function(item){
+		//alert(item.text);
+		var newList = this.state.data.concat([{title: item.title, text: item.text}])
+		this.setState({data: newList});
+	},
+	render: function(){
+		return(
+			<div>
+				<List data={this.state.data} handleDelete={this.handleDelete}/>
+				<Form onItemSubmit={this.handleSubmit}/>
+			</div>
+		);
+	}
+})
+
 
 
 export default App;
