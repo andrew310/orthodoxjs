af54df33f168e48716f2d8abfc822ed331c9bf40
Andrew Brown <andrewbrown600@gmail.com>
2016-06-23 20:47:21 -0700

    reverted line 3

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import App from 'components/app';
 
 ReactDOM.render(<div>REACT RENDERED AWESOMENESS!!</div>, document.getElementById('app'));
