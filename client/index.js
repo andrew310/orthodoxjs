ccffefc04dbe11effaf03f49da22b9b78c9b1062
Andrew Brown <andrewbrown600@gmail.com>
2016-06-23 20:33:18 -0700

    added components

---
+++ client/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'components/app';
+
+ReactDOM.render(<div>REACT RENDERED AWESOMENESS!!</div>, document.getElementById('app'));
