7dab326c24353303ca64a9de893477f55f83bdde
Andrew Brown <andrewbrown600@gmail.com>
2016-06-23 21:30:21 -0700

    server module added

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,25 @@
 const path = require('path');
 const CLIENT_DIR = path.resolve(__dirname, 'client');
+const SERVER_DIR = path.resolve(__dirname, 'server/generated');
+const DIST_DIR = path.resolve(__dirname, 'dist');
 
-module.exports = {
+const loaders: [{
+    test: /\.js$/,
+    include: CLIENT_DIR,
+    loader: 'babel-loader',
+    query: {
+      presets: ['es2015', 'react']
+    }
+  },
+  {
+    test: /\.less$/,
+    loader: 'style-loader!css-loader!less-loader'
+  }]
+
+
+module.exports = [{
+  name: 'client',
+  target: 'web',
   context: CLIENT_DIR,
   entry: './index.js',
   output: {
@@ -9,24 +27,36 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        include: CLIENT_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      },
-      {
-        test: /\.less$/,
-        loader: 'style-loader!css-loader!less-loader'
-      }
-    ]
+    loaders: loaders
   },
   resolve: {
     alias: {
       components: path.resolve(CLIENT_DIR, 'components')
     }
   }
-};
+},
+{
+  name: 'server',
+  target: 'node',
+  context: CLIENT_DIR,
+  entry: {
+    app: 'components/app/index.js'
+  },
+  output: {
+    path: SERVER_DIR,
+    filename: '[name].js',
+    libraryTarget: 'commonjs2'
+  },
+  externals: /^[a-z\-0-9]+$/,
+  module: {
+    loaders: loaders
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(CLIENT_DIR, 'components')
+    }
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ]
+}];
