5ec93de2aff1578c6b4484c24d7cc2a48e1c1a16
Andrew Brown <andrewbrown600@gmail.com>
2016-06-23 22:26:06 -0700

    changed render

--- client/index.js
+++ client/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/app';
 
-ReactDOM.render(<div>REACT RENDERED AWESOMENESS!!</div>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
