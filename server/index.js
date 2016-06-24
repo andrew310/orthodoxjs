467af7fb5b41ecec182ba23954ae6f7b99ddd4e4
Andrew Brown <andrewbrown600@gmail.com>
2016-06-23 21:38:42 -0700

    import react app

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import React from 'react';
+import ReactDomServer from 'react-dom/server';
+import App from './generated/app';
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('express server running');
+    res.send(ReactDomServer.renderToString(<App />));
 });
 
 app.listen(3000, () => console.log('magic happens on port 3000'));
